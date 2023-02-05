989f4a37994caca5ba147822d34ccfaede224d84
Pulkit <65671483+Pulkitxm@users.noreply.github.com>
2023-02-05 11:12:50 +0530

    Final Code

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.post("/", function (req, res) {
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+  var result = num1 + num2;
+  res.send("Your answer is " + result);
+});
+app.listen(9000, () => console.log("server started on port 3000"));
